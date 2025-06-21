73c174bbf80983864e2854ce6bc314bc4f771a2e
bata515 <longhui515@gmail.com>
2025-06-21 21:59:38 +0900

    HelloControllerの@RestController注釈からコメントを削除

--- backend/src/main/java/com/example/quizapp/controller/HelloController.java
+++ backend/src/main/java/com/example/quizapp/controller/HelloController.java
@@ -2,7 +2,7 @@ package com.example.quizapp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController  // JSONや文字列を返すAPI用のコントローラ
+@RestController
 public class HelloController {
 
     @GetMapping("/")
